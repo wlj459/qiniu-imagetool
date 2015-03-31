494454672b0300083f0ec23863d5df7a415fbdf8
wlj459 <wlj459@gmail.com>
2015-03-31 06:24:59 -0700

    官方更改了SDK，不开心

--- tool.py
+++ tool.py
@@ -1,41 +1,53 @@
 # -*- coding:utf-8 -*-
-import qiniu.conf
-import sys
+from qiniu import auth
 import os
 import qiniu.io
 import qiniu.rs
 import base64
 import Image
-from time import strftime,localtime
-def getname(name,size):
-	name='zq-image:'+name+'-'+size+image[image.find('.'):]
-	name=base64.encodestring(name)
-	return name
-def resize(image):
-	img=Image.open(image)
-	w,h=img.size
-	if(w>1000):
-		h=int(1000/float(w)*h)
-		w=1000
-		h=img.resize((w,h),Image.ANTIALIAS).save(image)
+from time import strftime, localtime
 
 
-qiniu.conf.ACCESS_KEY="hbpHhfXp27MvRDhgG6aTblX-FQsncR9SUOpSfFTn"
-qiniu.conf.SECRET_KEY="pl5-DWcb2O3RLRkjtGj1xX67VfOM6Q2HAlu-iG-7"
-while(True):
-	image=raw_input("directory")
-	resize(image)
-	name=strftime("%Y%m%d%H%M%S", localtime())
-	policy=qiniu.rs.PutPolicy('zq-image')
-	policy.saveKey=name+image[image.find('.'):] 
-	policy.persistentOps='imageView2/2/h/80|saveas/'+getname(name,'80')+';imageView2/2/h/160|saveas/'+getname(name,'160')+';imageView2/2/h/320|saveas/'+getname(name,'320')+';imageView2/2/h/640|saveas/'+getname(name,'640')
-	uptoken=policy.token()
-	extra=qiniu.io.PutExtra()
+class QiniuTool(self):
+    access_key = "hbpHhfXp27MvRDhgG6aTblX-FQsncR9SUOpSfFTn"
+    secret_key = "pl5-DWcb2O3RLRkjtGj1xX67VfOM6Q2HAlu-iG-7"
+    bucket_name = 'zq-image'
+    q = auth(access_key, secret_key)
 
-	ret,err=qiniu.io.put_file(uptoken,None,image,extra)
-	if err is  not None:
-		sys.stderr.write('error:%s' % err)
-	else:
-		print 'http://zq-image.qiniudn.com/'+name+image[image.find('.'):]  
+    def __init__(self, url, directory):
+        self.url = url
+        self.directory = directory
+
+    @staticmethod
+    def get_name(name, size):
+        name = 'zq-image:'+name+'-'+size+image[image.find('.'):]
+        name = base64.encodestring(name)
+        return name
+
+    def resize(self):
+        img = Image.open(self.directory)
+        w, h = img.size
+        if w > 1000:
+            h = int(1000/float(w)*h)
+            w = 1000
+            h = img.resize((w, h), Image.ANTIALIAS).save(image)
+
+    def work(self):
+        while True:
+            self.resize()
+            name = strftime("%Y%m%d%H%M%S", localtime())
+            policy = qiniu.rs.PutPolicy('zq-image')
+            policy.saveKey = name+image[image.find('.'):]
+            policy.persistentOps = 'imageView2/2/h/80|saveas/' + self.get_name(name, '80') + \
+                                   ';imageView2/2/h/160|saveas/' + self.get_name(name, '160') + \
+                                   ';imageView2/2/h/320|saveas/' + self.get_name(name, '320') + \
+                                   ';imageView2/2/h/640|saveas/' + self.get_name(name, '640')
+            uptoken = policy.token()
+            extra = qiniu.io.PutExtra()
+            ret, err = qiniu.io.put_file(uptoken, None, image, extra)
+            if err is not None:
+                sys.stderr.write('error:%s' % err)
+            else:
+                print self.url + name + image[image.find('.'):]
 
 
